36a60a35b306c7aa0da5468c2a192d9dc42adc76
profitah <qwer120542@gmail.com>
2023-10-18 06:41:16 +0900

    git test

---
+++ 이윤아/gdsc_1010task/src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react';
+import Comportent from './Comportent'; // Comportent 컴포넌트를 가져옵니다.
+
+  function App() {
+    return (
+      <div id="root"> 
+        <div className="Container">
+          <Comportent /> 
+        </div>
+      </div>
+    );
+  }
+  
+
+
+
+  export default App;
+
+
+/* 참고 자료
+1. gdsc 프론트엔드 강의자료 (좋은 자료 만들어주신 코어님들 늘 감사합니다!)
+2. https://youtu.be/QR1qiUqZg3A?si=xy4ktzR6XaXHaJ-y
+3. https://www.codeit.kr/community/questions/UXVlc3Rpb246NjM5ODQzYmQ0MTM1NWEyNjhmNmE0NTBi
+4. https://github.com/Egnoel/React-TypeScript-Todo/blob/master/src/App.tsx
+5. https://youtu.be/bjnW2NLAofI?si=iZa-g1UDSc6bWsG5
+6. https://velog.io/@hamham/리액트로-ToDo앱-만들기2-추가편
+7. 리엑트 교과서 (useState 관련 부분)
+8. https://velog.io/@somda/TypeScript-React로-todo-list-만들기1
+또 뭐있더라 악
+*/
---
+++ 이윤아/gdsc_1010task/src/Comportent.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { ListItem } from './Interface';
+import './App.tsx';
+
+function Comportent() {
+    // useState 2개 정의
+    // 1. input 값 관리
+    let [inputValue, setInputValue] = useState('');
+    // 2. 할일을 리스트 목록으로 담음
+    let [NewList, setNewList] = useState<ListItem[]>([]);
+
+    // 할 일을 추가하는 함수
+    let addTodo = () => {
+        if (inputValue) {
+            const newTodo: ListItem = {
+                isCheck: false,
+                todoTxt: inputValue,
+            };
+            setNewList([...NewList, newTodo]);  // 함수 사용시 값 변화
+            setInputValue(''); // 초기화
+        }
+    };
+
+    // 할 일을 삭제하는 함수
+    let removeTodo = (index: number) => {
+        let updatedList = [...NewList];
+        updatedList.splice(index, 1);
+        setNewList(updatedList); // 함수 사용시 값 변화
+    };
+
+    return (
+        <div>
+            <p className="Title">Yuna.I&nbsp; &nbsp;Todo List</p>
+
+            <div className="Input">
+                <input
+                    type="text"
+                    placeholder="오늘의 할 일은 무엇이 있을까요?"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button className="AddBtn" onClick={addTodo}>
+                    추가
+                </button>
+            </div>
+            <div className="TodoList">
+                {NewList.map((ToDo, InDex) => (
+                    <div key={InDex} className="TodoItem">
+                        <input
+                            type="checkbox"
+                            checked={ToDo.isCheck}
+                            onChange={() => {
+                                let UpdateList = [...NewList];
+                                UpdateList[InDex].isCheck = !ToDo.isCheck;
+                                setNewList(UpdateList);
+                            }}
+                        />
+                        <span className={ToDo.isCheck ? 'Checked' : ''}>{ToDo.todoTxt}</span>
+                        <button className="RemoveBtn" onClick={() => removeTodo(InDex)}>
+                            삭제
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+
+export default Comportent;
