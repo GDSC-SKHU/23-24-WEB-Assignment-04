acd4c885c9f96d1ba0b38dc7e63fe572b2f4013b
HarrySeop <parkpjs3083@naver.com>
2023-10-28 04:28:24 +0900

    feat: TodoItem 기능 구현

--- 박지섭/src/components/TodoItem.tsx
+++ 박지섭/src/components/TodoItem.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
+import ListItem from "./ListItem";
+
 function TodoItem() {
+  const [todoList, setTodoList] = useState<ListItem[]>([
+    {
+      isCheck: false,
+      todoTxt: "이게 맞는건가",
+    },
+    {
+      isCheck: true,
+      todoTxt: "이게되네",
+    }
+  ]);
+  
+  const removeTodo = (index: number) => {
+    const removeList = [...todoList];
+    removeList.splice(index, 1);
+    setTodoList(removeList);
+  }
+
   return (
     <div className="TodoList">
-      <div className="TodoItem">
-        <input className="inputCheck" type="checkbox" />
-        <span className="TodoText">살려주세요</span>
-        <button className="removeButton">삭제</button>
-      </div>
-      <div className="TodoItem">
-        <input className="inputCheck" type="checkbox" />
-        <span className="TodoText">살려주세요</span>
-        <button className="removeButton">삭제</button>
-      </div>
-    </div>
+      {todoList.map((item, index) =>(
+        <div className="TodoItem" key={index}>
+          <input className="inputCheck"
+            type="checkbox"
+            checked={ item.isCheck } />
+          <span className="TodoText">{ item.todoTxt }</span>
+          <button className="removeButton" onClick={() => removeTodo(index)} >삭제</button>
+        </div>
+      ))}
+    </div> 
   );
 }
 export default TodoItem;
