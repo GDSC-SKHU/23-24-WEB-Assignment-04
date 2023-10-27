3980f4fd70124a0554dc65cda27a2d7c80f2c5fe
HarrySeop <parkpjs3083@naver.com>
2023-10-28 03:08:56 +0900

    Design: App.tsx 레이아웃 완료
    과제 조건이랑 유사하게 스타일 만들기 완료

--- 박지섭/src/App.tsx
+++ 박지섭/src/App.tsx
@@ -8,13 +8,23 @@ function App() {
     <div className="root">
       <div className="Container">
         <p className="Title">
-
+          JiSeop.Park Todo List
         </p>
         <div className="Input">
-
+          <input className="inputText" type="text" placeholder="오늘 할일은 무엇이 있을까요?" />
+          <button className="addButton">추가</button>
         </div>
         <div className="TodoList">
-          
+          <div className="TodoItem">
+            <input className="inputCheck" type="checkbox" />
+            <span className="TodoText">살려주세요</span>
+            <button className="removeButton">삭제</button>
+          </div>
+          <div className="TodoItem">
+            <input className="inputCheck" type="checkbox" />
+            <span className="TodoText">살려주세요</span>
+            <button className="removeButton">삭제</button>
+          </div>
         </div>
       </div>
     </div>
