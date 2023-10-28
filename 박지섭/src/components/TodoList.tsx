a34533a189b44e33b838256417a353914c0baeac
HarrySeop <parkpjs3083@naver.com>
2023-10-28 23:46:31 +0900

    Refactor: 컴포넌트 분리 포기..

--- 박지섭/src/components/TodoList.tsx
+++ 박지섭/src/components/TodoList.tsx
@@ -1,14 +1,63 @@
-import AddTodo from './AddTodo';
-import TodoItem from './TodoItem';
+import { useState } from 'react';
+// import AddTodo from './AddTodo';
+// import TodoItem from './TodoItem';
+import ListItem from './ListItem';
 
 function TodoList() {
+  const [inputText, setInputText] = useState("");
+  const [todoList, setTodoList] = useState<ListItem[]>([
+    {
+      isCheck: false,
+      todoTxt: "이게 맞는건가",
+    },
+    {
+      isCheck: true,
+      todoTxt: "이게되네",
+    }
+  ]);
+
+  const addTodo = () => {
+    if (inputText.length !== 0) {
+      const newTodo: ListItem = {
+        isCheck: false,
+        todoTxt: inputText,
+      };
+      setTodoList([...todoList, newTodo]);
+      setInputText("");
+    }
+  };
+
+  const removeTodo = (index: number) => {
+    const removeList = [...todoList];
+    removeList.splice(index, 1);
+    setTodoList(removeList);
+  }
+
   return (
     <div className="Container">
       <p className="Title">
         JiSeop.Park Todo List
       </p>
-      <AddTodo />
-      <TodoItem />
+      <div className="Input">
+      <input className="inputText"
+        type="text"
+        placeholder="오늘 할일은 무엇이 있을까요?"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button className="addButton" onClick={ addTodo }>추가</button>
+    </div>
+      <div className="TodoList">
+      {todoList.map((item, index) =>(
+        <div className="TodoItem" key={index}>
+          <input className="inputCheck"
+            type="checkbox"
+            checked={ item.isCheck } />
+          <span className="TodoText">{ item.todoTxt }</span>
+          <button className="removeButton" onClick={() => removeTodo(index)} >삭제</button>
+        </div>
+      ))}
+    </div> 
     </div>
   );
 }
